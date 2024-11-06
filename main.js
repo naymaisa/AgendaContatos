b54a9f934f87dcb325f82f084b86c431265381b1
Nayara <nayaramaisa.stn@gmail.com>
2024-11-06 15:17:46 -0300

    alteraçoes finais

--- main.js
+++ main.js
@@ -1,25 +1,32 @@
 const form = document.getElementById('form-contato');
+const inputNomeContato = document.getElementById('nome-contato');
+const inputTelefone = document.getElementById('telefone');
+const inputEmail = document.getElementById('email');
 let linhas = '';
+const telefonesAdicionados = [];
 
 form.addEventListener('submit', function (e) {
     e.preventDefault();
 
-    const inputNomeContato = document.getElementById('nome-contato');
-    const inputTelefone = document.getElementById('telefone');
-    const inputEmail = document.getElementById('email');
+    if (telefonesAdicionados.includes(inputTelefone.value)) {
+        alert(`Este numero : ${inputTelefone.value} ja foi adionado anteriormente.`);
+    } else {
+
+        telefonesAdicionados.push(inputTelefone.value);
+
+        let linha = '<tr>';
+        linha += `<td>${inputNomeContato.value}</td>`;
+        linha += `<td>${inputTelefone.value}</td>`;
+        linha += `<td>${inputEmail.value}</td>`;
+        linha += `</tr>`;
+
+        linhas += linha;
+
+        const corpoTabela = document.querySelector(`tbody`);
+        corpoTabela.innerHTML = linhas;
+    }
 
 
-
-    let linha = '<tr>';
-    linha += `<td>${inputNomeContato.value}</td>`;
-    linha += `<td>${inputTelefone.value}</td>`;
-    linha += `<td>${inputEmail.value}</td>`;
-    linha += `</tr>`;
-
-    linhas += linha;
-
-    const corpoTabela = document.querySelector(`tbody`);
-    corpoTabela.innerHTML = linhas;
 
     inputNomeContato.value = '';
     inputTelefone.value = '';
