0980cb52bcbe772f4ee6da03e5bac929ba894205
Nayara <nayaramaisa.stn@gmail.com>
2024-11-06 14:46:02 -0300

    js funcionando

---
+++ main.js
@@ -0,0 +1,24 @@
+const form = document.getElementById('form-contato');
+let linhas = '';
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    const inputNomeContato = document.getElementById('nome-contato');
+    const inputTelefone = document.getElementById('telefone');
+    const inputEmail = document.getElementById('email');
+
+
+
+    let linha = '<tr>';
+    linha += `<td>${inputNomeContato.value}</td>`;
+    linha += `<td>${inputTelefone.value}</td>`;
+    linha += `<td>${inputEmail.value}</td>`;
+    linha += `</tr>`;
+
+    linhas += linha;
+
+    const corpoTabela = document.querySelector(`tbody`);
+    corpoTabela.innerHTML = linhas;
+
+});
